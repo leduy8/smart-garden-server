42cc9195021f52afba8b1b684a97f1a1e5b59fed
duyle <duy.leduc8@gmail.com>
2020-09-26 16:28:02 +0700

    add new branch for testing

--- index.js
+++ index.js
@@ -4,19 +4,28 @@ const mongoose = require("mongoose");
 const sensor = require("./routes/sensors");
 
 app.use(express.json());
-app.use("/api/sensor", sensor);
+app.use("/api/sensors", sensor);
 
 app.get("/", (req, res) => {
   const parsed = JSON.parse('{"testPassed": true}');
   res.send(parsed);
 });
 
-mongoose.connect(
-  "mongodb+srv://duy123:duy123456@cluster0.zt66n.mongodb.net/smart-garden?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+if ((process.env.NODE_ENV = "test")) {
+  mongoose.connect(
+    "mongodb+srv://duy123:duy123456@cluster0.zt66n.mongodb.net/smart-garden-test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+} else {
+  mongoose.connect(
+    "mongodb+srv://duy123:duy123456@cluster0.zt66n.mongodb.net/smart-garden?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+}
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => console.log(`Connected to port ${port}`));
+const server = app.listen(port, () =>
+  console.log(`Connected to port ${port}...`)
+);
 
 module.exports = server;
--- tests/sensor.test.js
+++ tests/sensor.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const { Sensor } = require("../models/sensor");
 let server;
 
 describe("/api/sensor", () => {
@@ -15,5 +16,27 @@ describe("/api/sensor", () => {
       expect(res.status).toBe(200);
       expect(res.body).toBeDefined();
     });
+  });
+
+  describe("POST /", () => {
+    let temperature;
+    let airHumidity;
+    let soilMoisture;
+    let timestamp;
+
+    const exec = async () => {
+      return await request(server)
+        .post("/api/sensors")
+        .send({ temperature, airHumidity, soilMoisture, timestamp });
+    };
+
+    beforeEach(() => {
+      temperature = 30;
+      airHumidity = 65;
+      soilMoisture = 45.6;
+      timestamp = new Date().getTime().toString();
+    });
+
+    it("should return 400 if temperature is NaN", async () => {});
   });
 });
