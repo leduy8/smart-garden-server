37badfdd4b50d644f9766d9b0e54e78e4c54c1c4
Vu Huy <vuhuycto@gmail.com>
2020-09-27 11:49:35 +0700

    test arduino communication

--- index.js
+++ index.js
@@ -4,30 +4,36 @@ const mongoose = require("mongoose");
 const sensor = require("./routes/sensors");
 const plantStatus = require("./routes/plantStatuses");
 
+// Arduino communication
+const Arduino = require("./models/arduino");
+const arduino = new Arduino("/dev/ttyACM0");
+
+console.log(arduino.getSensorsData());
+
 app.use(express.json());
 app.use("/api/sensors", sensor);
 app.use("/api/plantStatus", plantStatus);
 
 app.get("/", (req, res) => {
-  const parsed = JSON.parse('{"testPassed": true}');
-  res.send(parsed);
+	const parsed = JSON.parse('{"testPassed": true}');
+	res.send(parsed);
 });
 
 if ((process.env.NODE_ENV = "test")) {
-  mongoose.connect(
-    "mongodb+srv://duy123:duy123456@cluster0.zt66n.mongodb.net/smart-garden-test?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  );
+	mongoose.connect(
+		"mongodb+srv://duy123:duy123456@cluster0.zt66n.mongodb.net/smart-garden-test?retryWrites=true&w=majority",
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	);
 } else {
-  mongoose.connect(
-    "mongodb+srv://duy123:duy123456@cluster0.zt66n.mongodb.net/smart-garden?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  );
+	mongoose.connect(
+		"mongodb+srv://duy123:duy123456@cluster0.zt66n.mongodb.net/smart-garden?retryWrites=true&w=majority",
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	);
 }
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
-  console.log(`Connected to port ${port}...`)
+	console.log(`Connected to port ${port}...`)
 );
 
 module.exports = server;
