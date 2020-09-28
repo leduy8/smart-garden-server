609361d5e0cb254793ec66a3a627c1e693b2df26
Vu Huy <vuhuycto@gmail.com>
2020-09-28 15:57:13 +0700

    change autoDelayTime

--- utils/automatedTimeout.js
+++ utils/automatedTimeout.js
@@ -2,7 +2,7 @@ let autoDelayTime = 0;
 
 function setAutomatedTimeout(callback) {
 	setTimeout(callback, autoDelayTime);
-	autoDelayTime += 20000;
+	autoDelayTime += 10000;
 }
 
 module.exports.setAutomatedTimeout = setAutomatedTimeout;
