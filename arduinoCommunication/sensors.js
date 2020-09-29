467dbf7a010fae3d3f93718184ef5c80493bb0c7
duyle <duy.leduc8@gmail.com>
2020-09-29 10:41:10 +0700

    before adding selenium

--- arduinoCommunication/sensors.js
+++ arduinoCommunication/sensors.js
@@ -3,23 +3,6 @@ const { Arduino } = require("../models/arduino");
 const { setAutomatedTimeout } = require("../utils/automatedTimeout");
 const arduino = new Arduino("/dev/ttyACM0");
 
-// let sensorsData = null;
-// let pumpWaterFinishedNoti = null;
-
-//   setAutomatedTimeout(() => {
-//     arduino.getSensorsData((responseData) => {
-//       sensorsData = responseData;
-//       console.log(sensorsData);
-//     });
-//   })
-
-// setAutomatedTimeout(() => {
-// 	arduino.pumpWater((responseData) => {
-// 		pumpWaterFinishedNoti = responseData;
-// 		console.log(pumpWaterFinishedNoti);
-// 	});
-// });
-
 module.exports.getSensorsData = function (io) {
   io.on("connect", (socket) => {
     socket.on("getSensorsData", () => {
