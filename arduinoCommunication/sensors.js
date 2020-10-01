2d91493a9db0c4f4c09d2d3e85705294b07ca66c
duyle <duy.leduc8@gmail.com>
2020-10-01 16:26:09 +0700

    debug arduino init

--- arduinoCommunication/sensors.js
+++ arduinoCommunication/sensors.js
@@ -2,11 +2,15 @@ const { setAutomatedTimeout } = require("../utils/automatedTimeout");
 const { ArduinoCommunication } = require("./init");
 
 const arduino = ArduinoCommunication.getInstance();
+console.log(arduino);
 
 module.exports.getSensorsData = function (io) {
+  console.log(arduino);
   io.on("connect", (socket) => {
+    console.log(arduino);
     socket.on("getSensorsData", () => {
       setAutomatedTimeout(() => {
+        console.log(arduino);
         arduino.getSensorsData((responseData) => {
           socket.emit("returnSensorsData", responseData);
         });
