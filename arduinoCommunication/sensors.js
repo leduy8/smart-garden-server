ad65d644ab4c4ad7b3c80e3badd551173ceb458b
duyle <duy.leduc8@gmail.com>
2020-10-02 21:28:38 +0700

    add axios to test external service

--- arduinoCommunication/sensors.js
+++ arduinoCommunication/sensors.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const { setAutomatedTimeout } = require("../utils/automatedTimeout");
 const { ArduinoCommunication } = require("./init");
 
@@ -6,16 +7,21 @@ const arduino = ArduinoCommunication.getInstance();
 module.exports.getSensorsData = function (io) {
   io.on("connect", (socket) => {
     socket.on("getSensorsData", () => {
-      setAutomatedTimeout(() => {
-        arduino.getSensorsData((responseData) => {
-          socket.emit("returnSensorsData", responseData);
-        });
-      });
+      // setAutomatedTimeout(() => {
+      //   arduino.getSensorsData((responseData) => {
+      //     socket.emit("returnSensorsData", responseData);
+      //   });
+      // });
       // socket.emit("returnSensorsData", {
       //   temperature: 26,
       //   humidity: 60,
       //   soilMoisture: 69,
       // });
+      axios
+        .get("http://localhost:3001/api/sensors")
+        .then((response) => response.json())
+        .then((data) => socket.emit("returnSensorsData", data))
+        .catch((error) => console.log(error));
     });
   });
 };
