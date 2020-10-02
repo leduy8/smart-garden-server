57f6460d1524df7a6d2496ecfbeab2817dbf5451
duyle <duy.leduc8@gmail.com>
2020-10-02 22:19:41 +0700

    remove automated timeout

--- arduinoCommunication/sensors.js
+++ arduinoCommunication/sensors.js
@@ -1,28 +1,12 @@
 const axios = require("axios");
-const { setAutomatedTimeout } = require("../utils/automatedTimeout");
-//const { ArduinoCommunication } = require("./init");
-
-//const arduino = ArduinoCommunication.getInstance();
 
 module.exports.getSensorsData = function (io) {
   io.on("connect", (socket) => {
     socket.on("getSensorsData", () => {
-      // setAutomatedTimeout(() => {
-      //   arduino.getSensorsData((responseData) => {
-      //     socket.emit("returnSensorsData", responseData);
-      //   });
-      // });
-      // socket.emit("returnSensorsData", {
-      //   temperature: 26,
-      //   humidity: 60,
-      //   soilMoisture: 69,
-      // });
-      setAutomatedTimeout(() =>
-        axios
-          .get("http://localhost:3001/api/sensors")
-          .then((response) => socket.emit("returnSensorsData", response.data))
-          .catch((err) => console.log(err))
-      );
+      axios
+        .get("http://localhost:3001/api/sensors")
+        .then((response) => socket.emit("returnSensorsData", response.data))
+        .catch((err) => console.log(err));
     });
   });
 };
@@ -30,18 +14,10 @@ module.exports.getSensorsData = function (io) {
 module.exports.pumpWater = function (io) {
   io.on("connect", (socket) => {
     socket.on("pumpWaterRequest", () => {
-      // setAutomatedTimeout(() => {
-      //   arduino.pumpWater((responseData) => {
-      //     socket.emit("pumpWaterResponse", responseData);
-      //   });
-      // });
-      // socket.emit("pumpWaterResponse", { message: "Done pumping!" });
-      setAutomatedTimeout(() =>
-        axios
-          .get("http://localhost:3001/api/pumpWater")
-          .then((response) => socket.emit("pumpWaterResponse", response.data))
-          .catch((err) => console.log(err))
-      );
+      axios
+        .get("http://localhost:3001/api/pumpWater")
+        .then((response) => socket.emit("pumpWaterResponse", response.data))
+        .catch((err) => console.log(err));
     });
   });
 };
