ca1e65a1c9b75511318dfddc833749f26e27de66
duyle <duy.leduc8@gmail.com>
2020-10-12 17:15:54 +0700

    add test for arduino communication api

--- api/sensorsApi.js
+++ api/sensorsApi.js
@@ -4,7 +4,7 @@ const { setAutomatedTimeout } = require("./../utils/automatedTimeout");
 module.exports.getSensorsData = function (callback, error) {
   setAutomatedTimeout(() =>
     axios
-      .get("http://localhost:3001/api/sensors")
+      .get("http://localhost:3001/api/sensor")
       .then((response) => callback(response))
       .catch((err) => error(err))
   );
@@ -13,7 +13,7 @@ module.exports.getSensorsData = function (callback, error) {
 module.exports.pumpWater = function (callback, error) {
   setAutomatedTimeout(() =>
     axios
-      .get("http://localhost:3001/api/pumpWater")
+      .get("http://localhost:3001/api/pumpWaters")
       .then((response) => callback(response))
       .catch((err) => error(err))
   );
