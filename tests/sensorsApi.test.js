d9cd61f12c2a05bc2cad43dc8fbf246d6bfadb57
duyle <duy.leduc8@gmail.com>
2020-10-13 10:24:54 +0700

    correct test name

--- tests/sensorsApi.test.js
+++ tests/sensorsApi.test.js
@@ -2,7 +2,7 @@ const { getSensorsData, pumpWater } = require("../api/sensorsApi");
 
 describe("arduino communication service api", () => {
   describe("GET /api/sensors", () => {
-    it("should have all 3 properties of sensors data", async () => {
+    it("should have all 3 properties of sensors data or should have an error message if calling to communication service is failed", async () => {
       getSensorsData(
         (response) => {
           expect(response.data).toHaveProperty("temperature");
@@ -15,7 +15,7 @@ describe("arduino communication service api", () => {
   });
 
   describe("GET /api/pumpWater", () => {
-    it("should have message property", async () => {
+    it('should have a "Done" message property or should have an error message if calling to communication service is failed', async () => {
       pumpWater(
         (response) => {
           expect(response.data).toHaveProperty("message");
