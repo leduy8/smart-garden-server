e4dcc467f3a5e0cfbe8840aeb3aabe0d00220334
duyle <duy.leduc8@gmail.com>
2020-10-22 15:22:47 +0700

    get sensor data realtime with gpio

--- api/sensorsApi.js
+++ api/sensorsApi.js
@@ -1,20 +1,15 @@
 const axios = require("axios");
-const { setAutomatedTimeout } = require("./../utils/automatedTimeout");
 
 module.exports.getSensorsData = function (callback, error) {
-  setAutomatedTimeout(() =>
-    axios
-      .get("http://localhost:3001/api/sensors")
-      .then((response) => callback(response))
-      .catch((err) => error(err))
-  );
+  axios
+    .get("http://localhost:3001/api/sensors")
+    .then((response) => callback(response))
+    .catch((err) => error(err));
 };
 
 module.exports.pumpWater = function (callback, error) {
-  setAutomatedTimeout(() =>
-    axios
-      .get("http://localhost:3001/api/pumpWater")
-      .then((response) => callback(response))
-      .catch((err) => error(err))
-  );
+  axios
+    .get("http://localhost:3001/api/pumpWater")
+    .then((response) => callback(response))
+    .catch((err) => error(err));
 };
